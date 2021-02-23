8e962147c3c4863d031ee255819834a2867bb7dc
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-23 16:33:58 -0500

    testing initial methods to store object properties

---
+++ index.js
@@ -0,0 +1,19 @@
+// Import all class/subclasses
+const Employee = require("./lib/Employee");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+
+// Import all node libraries needed
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+
+//Write functions for each employee type that will run each method and store those properties then generate a new object
+
+
+
+// const init = () => {}
+
+
+// init();

79dc63590989abe1ebe007853d6049b25d37208a
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-22 21:16:04 -0500

    started writing unit tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,1 +1,13 @@
+const { expect } = require("@jest/globals");
+const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
+
+describe("Employee Class", () => {
+    describe("getName", () => {
+        it("should return a new employee name when called", () => {
+            const employee = new Employee();
+            const result = employee.getName();
+            expect(result).toEqual(res.name);
+        })
+    });
+});

129e0038935d84df3ca0977d49c16499e9f2c162
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-26 18:50:36 -0500

    testing initial prompt functions

--- index.js
+++ index.js
@@ -11,9 +11,108 @@ const util = require("util");
 
 //Write functions for each employee type that will run each method and store those properties then generate a new object
 
+function employeeSelection() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What type of employee would you like to add to your team profile next?",
+                name: "type",
+                choices: ["Engineer", "Intern", "Finish Building Team"]
+            }
+        ])
+        .then((res) => {
+            return res.type;
+        });
+}
+
+function createIntern() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the intern's name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the intern's employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the intern's email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What school does the intern attend?",
+                name: "school"
+            }
+        ])
+        .then((res) => {
+            const int = new Intern(res.name, res.id, res.email, res.school);
+        });
+}
+
+function createEngineer() {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the engineer's name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the engineer's github profile name?",
+                name: "github"
+            }
+        ])
+        .then((res) => {
+            const eng = new Engineer(res.name, res.id, res.email, res.github);
+        });
+}
 
 
-// const init = () => {}
+const init = () => {
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                message: "What is the team manager's name?",
+                name: "name",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's employee ID?",
+                name: "id",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's email address?",
+                name: "email",
+            },
+            {
+                type: "input",
+                message: "What is the team manager's office number?",
+                name: "office"
+            }
+        ])
+        .then((res) => {
+            const mgr = new Manager(res.name, res.id, res.email, res.office);
+            employeeSelection();
+        })
+}
 
-
-// init();
+init();

398ab95fe8241bb2361937462ca1fa3b4e5210d1
Sammy McMillan <74073409+smcmillan28@users.noreply.github.com>
2021-02-26 22:55:51 -0500

    added to Employee and Engineer unit tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,13 +1,39 @@
-const { expect } = require("@jest/globals");
-const { describe } = require("yargs");
 const Employee = require("../lib/Employee");
 
 describe("Employee Class", () => {
-    describe("getName", () => {
-        it("should return a new employee name when called", () => {
-            const employee = new Employee();
-            const result = employee.getName();
-            expect(result).toEqual(res.name);
+    describe("New Employee", () => {
+        it("should create a new Employee object with name of 'Sam'", () => {
+            const employee = new Employee("Sam", "2", "smcmillan@yahoo.com");
+            
+            expect(employee.name).toEqual("Sam");
+        });
+
+        it("should return the new Employee's name when getName() method is called", () => {
+            const employee = new Employee("Sam", "2", "smcmillan@yahoo.com");
+            const empName = employee.getName();
+
+            expect(empName).toEqual("Sam");
+        });
+
+        it("should return the new Employee's ID when getId() method is called", () => {
+            const employee = new Employee("Sam", "2", "smcmillan@yahoo.com");
+            const empID = employee.getId();
+
+            expect(empID).toEqual("2");
+        });
+
+        it("should return the new Employee's email when getEmail() method is called", () => {
+            const employee = new Employee("Sam", "2", "smcmillan@yahoo.com");
+            const empEmail = employee.getEmail();
+
+            expect(empEmail).toEqual("smcmillan@yahoo.com");
+        });
+
+        it("should return 'Employee' when getRole() method is called", () => {
+            const employee = new Employee("Sam", "2", "smcmillan@yahoo.com");
+            const empRole = employee.getRole();
+
+            expect(empRole).toEqual("Employee");
         })
     });
 });
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,1 +1,46 @@
 const Engineer = require("../lib/Engineer");
+
+describe("Engineer subclass", () => {
+    describe("New Engineer", () => {
+        it("should create a new Engineer object with name of 'Joe'", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com");
+            
+            expect(engineer.name).toEqual("Joe");
+        });
+
+        it("should return the new Employee's name when getName() method is called", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com");
+            const engName = engineer.getName();
+
+            expect(engName).toEqual("Joe");
+        });
+
+        it("should return the new Employee's ID when getId() method is called", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com");
+            const engID = engineer.getId();
+
+            expect(engID).toEqual("2");
+        });
+
+        it("should return the new Engineer's email when getEmail() method is called", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com");
+            const engEmail = engineer.getEmail();
+
+            expect(engEmail).toEqual("joe@yahoo.com");
+        });
+
+        it("should return 'Engineer' when getRole() method is called", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com");
+            const engRole = engineer.getRole();
+
+            expect(engRole).toEqual("Engineer");
+        })
+
+        it("should return Engineer's when GitHub profile is called", () => {
+            const engineer = new Engineer("Joe", "2", "joe@yahoo.com", "smcmillan28");
+            const engHub = engineer.github;
+
+            expect(engHub).toEqual("smcmillan28");
+        })
+    });
+});
